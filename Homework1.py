c346c22a623e91f6a4a43cfae6bb043f0e53893c
Natasha Kubiak <natashaskubiak@gmail.com>
2022-09-05 22:53:19 -0600

    Hiring problem

--- Homework1.py
+++ Homework1.py
@@ -1,6 +1,9 @@
 from itertools import permutations
 import time
 from tkinter import N
+import numpy as np
+from math import e
+
 
 
 def Factorial():
@@ -15,28 +18,29 @@ def Factorial():
 
 Array = [1,2,3,4,5,6,7,8]
 fact = Factorial()
-hire = 0                    # End count of Hires per permutation
-#Python Function that will create all permutation of the Array
-p = list(permutations(Array))
+ln8 = np.log(8)                 # assign the numpy log function to a new function called ln
+hire = 0                        # End count of Hires per permutation
+p = list(permutations(Array))   #Python Function that will create all permutation of the Array
 
 
 
 for x in p:                     # This gets every rand permutation of Array[1....8]
     best = 0                    # starting candidate is always hired
-    # print(x)                  # Just for checking visually
+    # print(x)                    # Just for checking visually
     for i in x:                 # Next we will grab each individual potential hire in the list
         # print(i)                # Just for checking visually
         if i > best:            # If candidate is better than the Hired person
             best = i            # Hire candidate
-            # print("Hire!!")   # Just for checking visually
+            # print("Hire!!")     # Just for checking visually
             hire = hire + 1     #Each time a HIRE occurs, add this to end count H1 + H2 +...Hn
             # print("hire=", hire)
 
 
 
 
-
+print(" ")
 print("Total Hires: ", hire)   
+print(" ")
 
 
 
@@ -44,9 +48,20 @@ print("Total Hires: ", hire)
 ############# CALCULATIONS ###############
 
 Expected = hire/fact            #theoretical expected value as EH = 1/8!(H1 + H2 + ... + H8!)
+Harmonic= 1 + 1/2 + 1/3 + 1/4 + 1/5 + 1/6 + 1/7 + 1/8
+# Variance = Summation(n) of (1/i)  - Summation(n) of (1/i^2)
+Variance = (1 + 1/2 + 1/3 + 1/4 + 1/5 + 1/6 + 1/7 + 1/8 ) - (1 + 1/4 + 1/9 + 1/16 + 1/25 + 1/36 + 1/49 + 1/64)
+
+# Variance = 
+
 #Var(H) = E(H^2) - (EH)^2 = 1/8!(H1^2 + H2^2 + ... + H81^2) - (EH)^2.
+print("-----EXPECTATION-----")
 print("Theoretical Expected Value:", Expected)
-print("Theoretical Variance:")
+print("Value discussed in class as nth value Harmonic: ", Harmonic)
+print("Value compared with ln(8): ", ln8)
+print(" ")
+print("------VARIANCE------")
+print("Theoretical Variance:", Variance)
 
 
 
